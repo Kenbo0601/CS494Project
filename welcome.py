ff513aba07dcfb454139877f3b388572ccde4346
Kenbo0601 <kenichi2@pdx.edu>
2020-04-27 19:17:07 -0700

    window size changed

--- welcome.py
+++ welcome.py
@@ -2,13 +2,14 @@ from tkinter import *
 
 window = Tk()
 window.title("Welcome to the ChatAPP")
-window.geometry("400x300")
+window.geometry("300x200")
 fm = Frame(window)
 
+label = Label(fm, text="***** MENU *****").pack(side=TOP)
 makeRoom = Button(fm, text='MAKE A ROOM').pack(side=TOP, expand=YES)
 makeRoom = Button(fm, text='JOIN A ROOM').pack(side=TOP,expand=YES)
 makeRoom = Button(fm, text='LEAVE', command=quit).pack(side=TOP,expand=YES)
-fm.pack(fill=BOTH,expand=YES)
+fm.pack(fill=BOTH)
 
 window.mainloop()
 
