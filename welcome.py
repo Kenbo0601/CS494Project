a4768c97c7a1528cb946656ab1e1ebf4717f6070
Kenbo0601 <kenichi2@pdx.edu>
2020-04-27 20:34:18 -0700

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,17 +1,42 @@
 from tkinter import *
 
+
+def make_room():
+    newWindow = Toplevel(window)
+    newWindow.geometry("300x200")
+    lablel2 = Label(newWindow, text="MAKE A ROOM")
+    fm = Frame(newWindow)
+
+    back_main = Button(fm, text="Main menu", command=main_menu).pack(side=TOP)
+    fm.pack(fill=BOTH)
+
+def join_room():
+    newWindow = Toplevel(window)
+    newWindow.geometry("300x200")
+    lablel2 = Label(newWindow, text="JOIN A ROOM")
+
+
+def main_menu():
+    newWindow = Toplevel(window)
+    newWindow.geometry("300x200")
+    fm = Frame(newWindow)
+    label = Label(fm,text="***** MENU *****").pack(side=TOP)
+    makeRoom = Button(fm,text='MAKE A ROOM', command=make_room).pack(side=TOP, expand=YES)
+    makeRoom = Button(fm,text='JOIN A ROOM', command=join_room).pack(side=TOP,expand=YES)
+    makeRoom = Button(fm,text='LEAVE', command=quit).pack(side=TOP,expand=YES)
+    fm.pack(fill=BOTH)
+
+
 window = Tk()
 window.title("Welcome to the ChatAPP")
 window.geometry("300x200")
 fm = Frame(window)
+main = Button(fm, text="main", command=main_menu).pack(side=TOP)
+fm.pack()
 
-label = Label(fm, text="***** MENU *****").pack(side=TOP)
-makeRoom = Button(fm, text='MAKE A ROOM').pack(side=TOP, expand=YES)
-makeRoom = Button(fm, text='JOIN A ROOM').pack(side=TOP,expand=YES)
-makeRoom = Button(fm, text='LEAVE', command=quit).pack(side=TOP,expand=YES)
-fm.pack(fill=BOTH)
 
-window.mainloop()
+window.mainloop();
+
 
 
 
