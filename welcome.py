18562d31402005885567adf351d17514f1e4aca4
Kenbo0601 <kenichi2@pdx.edu>
2020-04-28 12:54:57 -0700

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -21,6 +21,7 @@ def main_menu():
     newWindow.geometry("300x200")
     fm = Frame(newWindow)
     label = Label(fm,text="***** MENU *****").pack(side=TOP)
+    label2 = Label(fm,text="Hi," + str(userName.get())).pack()
     makeRoom = Button(fm,text='MAKE A ROOM', command=make_room).pack(side=TOP, expand=YES)
     makeRoom = Button(fm,text='JOIN A ROOM', command=join_room).pack(side=TOP,expand=YES)
     makeRoom = Button(fm,text='LEAVE', command=quit).pack(side=TOP,expand=YES)
@@ -31,8 +32,10 @@ window = Tk()
 window.title("Welcome to the ChatAPP")
 window.geometry("300x200")
 fm = Frame(window)
+userName = StringVar() #User variable
 label = Label(fm,text="Hello! Enter your name!").pack(side=TOP)
-main = Button(fm, text="main", command=main_menu).pack(side=TOP)
+entry = Entry(fm, textvariable=userName).pack()
+main = Button(fm, text="go to main", command=main_menu).pack(side=TOP)
 fm.pack()
 
 
